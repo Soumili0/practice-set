b5520e60c61ac4ad3c64f63e0fbc8b8afa920921
Soumili Samanta <soumili.samanta18@gmail.com>
2026-04-01 08:55:59 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LinearSearch {
+    public static int linearSearch(int []arr, int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i] = sc.nextInt();
+    }
+    int target = sc.nextInt();
+    int result = linearSearch(arr,target);
+
+    if(result != -1){
+        System.out.println("element found at index:"+result);
+    }else{
+        System.out.println("element not found");
+    }
+  }  
+}
