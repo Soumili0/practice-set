eef0ea4e7ec08fcdca476bd94a7aaaea5b5044af
Soumili Samanta <soumili.samanta18@gmail.com>
2026-04-02 19:30:17 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,40 @@
+/*import java.util.*;
+public class MissingNumber {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n-1];
+        for(int i=0;i<n-1;i++){
+            arr[i] = sc.nextInt();
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=1;i<=n;i++){
+            set.add(i);
+        }
+        for(int i=0;i<n-1;i++){
+            if(set.contains(arr[i])){
+                set.remove(arr[i]);
+            }
+        }
+        System.out.println(set);
+    }
+}
+*/
+import java.util.*;
+public class MissingNumber {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = n*(n+1)/2;
+        int arr[] = new int[n-1];
+        for(int i=0;i<n-1;i++){
+            arr[i]=sc.nextInt();        }
+        
+    int arrSum=0;
+    for(int i=0;i<n-1;i++){
+        arrSum+=arr[i];
+    }   
+int ans = sum-arrSum;
+System.out.println(ans);
+}
+}
