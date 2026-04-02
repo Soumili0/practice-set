6a0305e248d52d2193bb28b91d9b696f703b8ad0
Soumili Samanta <soumili.samanta18@gmail.com>
2026-04-02 20:21:35 +0530

    Add files via upload

---
+++ AppearNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class AppearNumber {
+    //using xor operator
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            ans=ans^nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        AppearNumber obj = new AppearNumber();
+        int ans = obj.singleNumber(arr);
+        System.out.println(ans);
+    }
+}
