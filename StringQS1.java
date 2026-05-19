470fde1d4d3f1e03657da14bc8d655815d5b33b3
Soumili Samanta <soumili.samanta18@gmail.com>
2026-05-19 19:44:25 +0530

    Create StringQS1.java

---
+++ StringQS1.java
@@ -0,0 +1,34 @@
+//String Palindrome or not
+import java.util.*;
+
+public class Main {
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // input string
+        String str = sc.nextLine();
+
+        if (isPalindrome(str)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
